bac73fc399804b430b1de332252c25989945bad6
Sebastian Nunez <02snunez@gmail.com>
2022-09-05 04:04:34 -0400

    module 4 added partially

---
+++ Zybook/4. Loops/4.2.3.c
@@ -0,0 +1,24 @@
+// Write a while loop that prints userNum divided by 2 (integer division) until reaching 1. Follow each number by a space. Example output for userNum = 40:
+// 20 10 5 2 1
+
+// Note: These activities may test code with different test values. This activity will perform four tests, with userNum = 40, then with userNum = 2, then with userNum = 0, then with userNum = -1. See "How to Use zyBooks".
+
+// Also note: If the submitted code has an infinite loop, the system will stop running the code after a few seconds, and report "Program end never reached." The system doesn't print the test case that caused the reported message.
+
+#include <stdio.h>
+
+int main(void) {
+    int userNum;
+
+    scanf("%d", &userNum);
+
+    /* Your solution goes here  */
+    while (userNum > 1) {
+        userNum /= 2;
+        printf("%d ", userNum);
+    }
+
+    printf("\n");
+
+    return 0;
+}
---
+++ Zybook/4. Loops/4.3.3.c
@@ -0,0 +1,20 @@
+// Given positive integer numInsects, write a while loop that prints that number doubled without reaching 100. Follow each number with a space. After the loop, print a newline. Ex: If numInsects = 8, print:
+// 8 16 32 64
+
+#include <stdio.h>
+
+int main(void) {
+    int numInsects;
+
+    scanf("%d", &numInsects);  // Must be >= 1
+
+    /* Your solution goes here  */
+    while (numInsects < 100) {
+        printf("%d ", numInsects);
+        numInsects *= 2;
+    }
+
+    printf("\n");
+
+    return 0;
+}
