458c8a4e981d85ae654c548afe6f2a566e41381a
Sebastian Nunez <02snunez@gmail.com>
2022-09-13 00:50:54 -0400

    module 7 done

---
+++ Zybooks/7. Structs/7.2.2.1.c
@@ -0,0 +1,31 @@
+// Write a statement that calls the function IncreaseItemQty with parameters notebookInfo and addStock. Assign
+// notebookInfo with the returned value.
+
+#include <stdio.h>
+
+typedef struct ProductInfo_struct {
+    char itemName[50];
+    int itemQty;
+} ProductInfo;
+
+ProductInfo IncreaseItemQty(ProductInfo productToStock, int increaseValue) {
+    productToStock.itemQty = productToStock.itemQty + increaseValue;
+
+    return productToStock;
+}
+
+int main(void) {
+    ProductInfo notebookInfo;
+    int addStock;
+
+    scanf("%s", notebookInfo.itemName);
+    scanf("%d", &notebookInfo.itemQty);
+    scanf("%d", &addStock);
+
+    /* Your code goes here */
+    notebookInfo = IncreaseItemQty(notebookInfo, addStock);
+
+    printf("Name: %s, stock: %d\n", notebookInfo.itemName, notebookInfo.itemQty);
+
+    return 0;
+}
