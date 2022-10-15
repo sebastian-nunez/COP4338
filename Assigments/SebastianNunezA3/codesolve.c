a71d03377ab3d62d3ff51d86ce1119995be8db00
Sebastian Nunez <02snunez@gmail.com>
2022-10-15 17:36:50 -0400

    A3. insert is not working

--- Assigments/SebastianNunezA3/codesolve.c
+++ Assigments/SebastianNunezA3/codesolve.c
@@ -14,11 +14,31 @@
  * - Note: input letters will be stored in a doubly linked list
  */
 
+#include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
+typedef struct Node {
+  char letter;
+  struct Node* prev;
+  struct Node* next;
+} Node;
+
+Node* createNode(void);
+
+typedef struct LinkedList {
+  Node* head;
+  Node* tail;
+  int size;
+} LinkedList;
+
+LinkedList* createLinkedList(void);
+void destroyLinkedList(LinkedList* list);
+void insertLinkedList(LinkedList* list, char letter);
+void printLinkedList(LinkedList* list);
 
 void solve(char inputFileName[], char outFileName[]);
 
@@ -45,9 +65,9 @@ int main(int argc, char* argv[]) {
     }
   }
   if ((optind + 1) != argc) {
-    // need at least one argument (change +1 to +2 for two, etc. as needeed)
+    // need at one argument for the input file
     printf("optind = %d, argc=%d\n", optind, argc);
-    fprintf(stderr, "%s: missing input_file_name\n", argv[0]);
+    fprintf(stderr, "%s: missing inputFileName\n", argv[0]);
     fprintf(stderr, usage, argv[0]);
     exit(1);
   } else if (err) {
@@ -56,7 +76,9 @@ int main(int argc, char* argv[]) {
   }
 
   inputFileName = argv[optind++];
+
   solve(inputFileName, outFileName);
+
   exit(0);
 }
 
@@ -75,13 +97,169 @@ void solve(char inputFileName[], char outFileName[]) {
     exit(100);
   }
 
+  LinkedList* list = createLinkedList();
+
   char inputChar;
   while (!feof(inFile)) {
     fscanf(inFile, " %c", &inputChar);
-
-    fprintf(outFile, "%c\n", inputChar);
+    inputChar = toupper(inputChar);
+    insertLinkedList(list, inputChar);
   }
 
+  Node* current = list->head->next;
+  for (int i = 0; i < list->size; ++i) {
+    fprintf(outFile, "%c", current->letter);
+    current = current->next;
+  }
+
+  destroyLinkedList(list);
   fclose(inFile);
   fclose(outFile);
+}
+
+Node* createNode() {
+  Node* newNode = (Node*)malloc(sizeof(Node));
+  newNode->letter = -1;
+  newNode->next = NULL;
+  newNode->prev = NULL;
+
+  return newNode;
+}
+
+LinkedList* createLinkedList() {
+  LinkedList* list = (LinkedList*)malloc(sizeof(LinkedList));  // create list
+
+  // create head and tail 'dummy' nodes
+  Node* head = createNode();
+  head->letter = '*';
+  Node* tail = createNode();
+  tail->letter = '*';
+
+  // place our dummy nodes in the list
+  list->head = head;
+  list->tail = tail;
+  list->size = 0;
+
+  // make the head's next point to the tail (and prev to NULL)
+  list->head->next = tail;
+  list->head->prev = NULL;
+
+  // make the tail's prev point to the head (and next to NULL)
+  list->tail->next = NULL;
+  list->tail->prev = head;
+
+  printf("LinkedList created!\n");
+  return list;
+}
+
+void destroyLinkedList(LinkedList* list) {
+  if (list == NULL) {
+    printf("List does not exist! Unable to destroy!\n");
+    return;
+  }
+
+  Node* current = list->head;
+  Node* next;
+  while (current) {
+    next = current->next;
+    printf("Node %c destroyed!\n", current->letter);
+    free(current);
+    current = next;
+  }
+
+  printf("LinkedList destroyed!\n");
+  free(list);
+  list = NULL;
+}
+
+// void insertLinkedList(LinkedList* list, char letter) {
+//   if (list == NULL) {
+//     printf("List does not exist! Unable to insert!\n");
+//     return;
+//   }
+
+//   // create newNode and give it the data
+//   Node* newNode = createNode();
+//   newNode->letter = letter;
+
+//   Node* chain = list->head->next;  // save the chain of nodes
+
+//   // fix newNode pointers
+//   newNode->next = chain;
+//   newNode->prev = list->head;
+
+//   // fix head pointers
+//   list->head->next = newNode;
+//   chain->prev = newNode;
+
+//   list->size++;
+//   printf("Added %c to the list!\n", letter);
+// }
+
+void insertLinkedList(LinkedList* list, char letter) {
+  if (list == NULL) {
+    printf("List does not exist! Unable to insert!\n");
+    return;
+  }
+
+  // create newNode and give it the data
+  Node* newNode = createNode();
+  newNode->letter = letter;
+
+  if (list->size == 0) {  // empty list. insert at the head
+    Node* chain = list->head->next;
+    newNode->next = chain;
+    newNode->prev = list->head;
+
+    list->head->next = newNode;
+    list->tail->prev = newNode;
+    list->size++;
+    return;
+  }
+
+  Node* current = list->head->next;  // save the chain of nodes
+  Node* prev = current->prev;        // store the previous node
+
+  for (int i = 0; i < list->size; ++i) {
+    if (letter == current->letter) {  // duplicate value. ignore!
+      return;
+    }
+
+    // position found, insert
+    if (letter < current->letter) {
+      Node* chain = current->next;  // save the chain
+      newNode->next = chain;
+      newNode->prev = prev;
+
+      prev->next = newNode;
+      chain->prev = newNode;
+
+      list->size++;
+      printf("Added %c to the list!\n", letter);
+      return;
+    }
+
+    prev = current;
+    current = current->next;
+  }
+}
+
+void printLinkedList(LinkedList* list) {
+  if (list == NULL) {
+    printf("List does not exist! Unable to print!\n");
+    return;
+  }
+
+  if (list->size == 0) {
+    printf("Empty list!\n");
+    return;
+  }
+
+  Node* current = list->head->next;
+  for (int i = 0; i < list->size; ++i) {
+    printf("%c ", current->letter);
+    current = current->next;
+  }
+
+  printf("\n");
 }
